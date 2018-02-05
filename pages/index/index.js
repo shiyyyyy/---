785044bfd34e04e4e4b53aaf8be30cd338846197
shiyyyyy <923450363@qq.com>
2018-02-05 18:22:48 +0800

    首页修复

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,45 +6,68 @@ Page({
   data: {
     // 轮播图
     imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
-      '../../images/0.jpg'
+      '../../images/banner/banner1.jpg',
+      '../../images/banner/banner2.jpg',
+      '../../images/banner/banner3.jpg',
+      '../../images/banner/banner4.jpg'
     ],
     indicatorDots: true,
     autoplay: true,
     interval: 5000,
     duration: 1000,
     current: true,
+    circular:true,
 
     // 小分类列表
-    tourism_type: { '旅游线路': '../../images/0.jpg', '欧美': '../../images/0.jpg', '东南亚': '../../images/0.jpg', '国内': '../../images/0.jpg', '蜜月游': '../../images/0.jpg', '亲子游': '../../images/0.jpg', '自由行': '../../images/0.jpg', '探险游': '../../images/0.jpg'},
+    tourism_type: { '旅游线路': '../../images/icon/1.jpg', '欧美': '../../images/icon/2.jpg', '东南亚': '../../images/icon/3.jpg', '国内': '../../images/icon/4.jpg', '蜜月游': '../../images/icon/5.jpg', '亲子游': '../../images/icon/6.jpg', '自由行': '../../images/icon/7.jpg', '探险游': '../../images/icon/8.jpg' },
 
     // 签证服务
     service: [
-      { img:"../../images/1.jpg", money: 4999, transport: "北京送签", text: "日本签证,旅游三年,多次往返,我要显示省略号", promotion: "元旦春节促销",countries: "日本", boundaries: "境外游" },
-      { img:"../../images/1.jpg", money: 4999, transport: "北京送签", text: "日本签证,旅游三年,多次往返,我要显示省略号", promotion: "元旦春节促销",countries: "日本", boundaries: "境外游" },
-      { img:"../../images/1.jpg", money: 4999, transport: "北京送签", text: "日本签证,旅游三年,多次往返,我要显示省略号", promotion: "元旦春节促销",countries: "日本", boundaries: "境外游" },
-      { img:"../../images/1.jpg", money: 4999, transport: "北京送签", text: "日本签证,旅游三年,多次往返,我要显示省略号", promotion: "元旦春节促销",countries: "日本", boundaries: "境外游" },]
+      { img: "../../images/1.jpg", money: 4999, transport: "北京送签", text: "日本签证,旅游三年,多次往返,我要显示省略号", promotion: "元旦春节促销", countries: "日本", boundaries: "境外游" },
+      { img: "../../images/1.jpg", money: 4999, transport: "北京送签", text: "日本签证,旅游三年,多次往返,我要显示省略号", promotion: "元旦春节促销", countries: "日本", boundaries: "境外游" },
+      { img: "../../images/1.jpg", money: 4999, transport: "北京送签", text: "日本签证,旅游三年,多次往返,我要显示省略号", promotion: "元旦春节促销", countries: "日本", boundaries: "境外游" },
+      { img: "../../images/1.jpg", money: 4999, transport: "北京送签", text: "日本签证,旅游三年,多次往返,我要显示省略号", promotion: "元旦春节促销", countries: "日本", boundaries: "境外游" }],
+    response:[]
   },
-  changeIndicatorDots: function (e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
+
+  // 加载触发 发送请求
+  onLoad: function (option) {
+    var that = this
+    wx.request({
+      url: 'http://localhost/test.json',
+      data: {},
+      method: 'GET',
+      success: function (res) {
+        // success
+        that.setData({
+          response: res.data
+        })
+        console.log(that.data.response)
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
     })
   },
-  changeAutoplay: function (e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
+  // input输入框改变事件
+  b_input_change: function (event) {
+    console.log(event)
   },
-  intervalChange: function (e) {
-    this.setData({
-      interval: e.detail.value
-    })
+
+  // 轮播图点击事件
+  c_swiper_tag: function (event) {
+
   },
-  durationChange: function (e) {
-    this.setData({
-      duration: e.detail.value
-    })
+  
+  // 点击 more 显示全部签证服务
+  c_more_tap: function() {
+
+  },
+  // 点击 签证服务主题内容 
+  b_visa_item_tap: function() {
+
   }
 })
